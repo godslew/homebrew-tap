d6024744ddf46b28e23d8fdc7b31fe41b2a5a778
GitHub Action <action@github.com>
2025-12-22 17:21:04 +0000

    Update gclip-cli to refs/tags/v0.1.2

--- Formula/gclip-cli.rb
+++ Formula/gclip-cli.rb
@@ -1,29 +1,9 @@
 class GclipCli < Formula
-  desc "Collect, search, and insert frequently used shell commands"
   homepage "https://github.com/godslew/gclip-cli"
-  url "https://github.com/godslew/gclip-cli/releases/download/v0.1.1/gclip-v0.1.1-macos.tar.gz"
-  sha256 "01f05e91d9cb9283c40af00a81f66a001cf1b149c9871f87b41d1490e2265317"
-  license "MIT"
-
-  depends_on :macos
+  url "https://github.com/godslew/gclip-cli/releases/download/v0.1.2/gclip-v0.1.2-macos.tar.gz"
+  sha256 "eb42187fb5c38a679fd525dac756f2b25601334f6beb650f9fa17f42a5c4a284"
 
   def install
     bin.install "gclip"
-  end
-
-  def caveats
-    <<~EOS
-      To enable the keybinding (Ctrl+g), add the following to your ~/.zshrc:
-
-        source <(gclip --init)
-
-      Then restart your shell or run:
-
-        source ~/.zshrc
-    EOS
-  end
-
-  test do
-    system "#{bin}/gclip", "--version"
   end
 end
