24d40e3cf594b51d08b2e9f94e1d30de8e26b7d0
GitHub Action <action@github.com>
2025-12-25 12:00:19 +0000

    Update gclip-cli to refs/tags/v0.1.5

--- Formula/gclip-cli.rb
+++ Formula/gclip-cli.rb
@@ -1,7 +1,7 @@
 class GclipCli < Formula
   homepage "https://github.com/godslew/gclip-cli"
-  url "https://github.com/godslew/gclip-cli/releases/download/v0.1.4/gclip-v0.1.4-macos.tar.gz"
-  sha256 "74bab76228ce20735b821e27af6fcf6454bdeb41285902b28bfb13214e533c3c"
+  url "https://github.com/godslew/gclip-cli/releases/download/v0.1.5/gclip-v0.1.5-macos.tar.gz"
+  sha256 "197f1207db51c1ac91cc378263658644592a6ccd6de45d8658128f070937eec4"
 
   def install
     bin.install "gclip"
